bec3ce68cfaee5cd74ec8edf2618fc9964fa51d6
RICHARD richard <ierdre44@gmail.com>
2018-01-17 14:28:18 +0100

    Initial Commit

---
+++ vonKoch2/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  vonKoch2
+//
+//  Created by richard urunuela on 17/01/2018.
+//  Copyright © 2018 richard urunuela. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
